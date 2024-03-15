31cac371244e0b3067fb3377d15adc605738ec1c
Javier <javierbengolea@yahoo.com.ar>
2024-03-15 08:25:00 -0300

    ---

--- main.py
+++ main.py
@@ -12,3 +12,9 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+    
+@app.get("/{nombre}")
+async def saludo(nombre: str):
+    saludo_st: str = f"Hello {nombre}!!!"
+    
+    return {"message": saludo_st}
